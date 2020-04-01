c28f2be54ac00f3a8dfce598f5469d645289e4d0
Felipe Delgado <af.delgador@uniandes.edu.co>
2020-03-31 22:52:08 -0500

    update

---
+++ scripts/dataCourses.js
@@ -0,0 +1,10 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de sw", "Rubby Casallas", 3),
+    new Course("Sistemas transaccionales", "AnaMaria Mojica", 3),
+    new Course("Infraestructura computacional", "Sandra Rueda", 3),
+    new Course("Diseño y Análisis de Algoritmos ", "Rodrigo Cardozo", 3),
+    new Course("Lenguajes y Maquinas", "Silvia Takahashi", 3),
+    new Course("BaloncestO MIXTO", "Ivan Rincón", 3),
+    new Course("COL: Espacio,tiempo y diferencia", "Carlos Oviedo", 2),
+];
